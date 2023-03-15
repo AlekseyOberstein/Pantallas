ca7067c63e7662581c6bdc8b6d87dbba702c0089
AlekseyOberstein <420035969@pcpuma.acatlan.unam.mx>
2023-03-15 12:21:04 -0700

    Initial Commit

---
+++ Pantallas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pantallas
+//
+//  Created by CEDAM01 on 15/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
